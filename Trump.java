3fa48b95e7622684d4ebe5df50e1f8159cc41967
kengo <92735653+zvhmsia@users.noreply.github.com>
2025-07-11 17:09:10 +0900

    Update Trump.java

--- Trump.java
+++ Trump.java
@@ -70,12 +70,12 @@ class Carddeck {
         System.out.println("");
     }
 
-    public static void sote_cards(List<List<String>> cards){
+    public static void sort_cards(List<List<String>> cards){
         int flag = 0;
         List<String> sort;
         int current;
         int next;
-        for(int j=4; j>0; j--){
+        for(int j=cards.size()-1; j>0; j--){
             for(int i=0; i<j; i++){
                 current=Integer.parseInt(cards.get(i).get(1));
                 next=Integer.parseInt(cards.get(i+1).get(1));
@@ -117,32 +117,46 @@ class Carddeck {
         }
     }
 
-    public static void sote_cards_n(List<List<String>> cards){
+    public static void sort_cards_suit(List<List<String>> cards){
         int flag = 0;
         List<String> sort;
-        
-        for(int j=4; j>0; j--){
+        int current=0;
+        int next=0;
+        for(int j=cards.size()-1; j>0; j--){
             for(int i=0; i<j; i++){
+                if(cards.get(i).get(0)!="joker" && cards.get(i+1).get(0)!="joker"){
+                    current=Integer.parseInt(cards.get(i).get(1));
+                    next=Integer.parseInt(cards.get(i+1).get(1));
+                    if(current==1){
+                        current += 13;
+                    }
+                    if(next==1){
+                        next += 13;
+                    }
+                }
                 switch(cards.get(i).get(0)){
                     case "spade":
-                        if((cards.get(i+1).get(0)=="spade") && ((Integer.parseInt(cards.get(i).get(1))) > (Integer.parseInt(cards.get(i+1).get(1))) && (Integer.parseInt(cards.get(i).get(1))!=1))){
+                        if((cards.get(i+1).get(0)=="spade") && (current > next)){
                             flag = 1;
                         }
                         break;
                     case "heart":
-                        if(cards.get(i+1).get(0)=="spade" || ((cards.get(i+1).get(0)=="heart") && ((Integer.parseInt(cards.get(i).get(1))) > (Integer.parseInt(cards.get(i+1).get(1)))) && (Integer.parseInt(cards.get(i).get(1))!=1))){
+                        if(cards.get(i+1).get(0)=="spade" || ((cards.get(i+1).get(0)=="heart") && (current > next))){
                             flag = 1;
                         }
                         break;
                     case "diamond":
-                        if(cards.get(i+1).get(0)=="spade" || cards.get(i+1).get(0)=="heart" || ((cards.get(i+1).get(0)=="heart") && ((Integer.parseInt(cards.get(i).get(1))) > (Integer.parseInt(cards.get(i+1).get(1)))) && (Integer.parseInt(cards.get(i).get(1))!=1))){
+                        if(cards.get(i+1).get(0)=="spade" || cards.get(i+1).get(0)=="heart" || ((cards.get(i+1).get(0)=="diamond") && (current > next))){
                             flag = 1;
                         }
                         break;
                     case "club":
-                        if(cards.get(i+1).get(0)=="spade" || cards.get(i+1).get(0)=="heart" || cards.get(i+1).get(0)=="diamond" || ((cards.get(i+1).get(0)=="heart") && ((Integer.parseInt(cards.get(i).get(1))) > (Integer.parseInt(cards.get(i+1).get(1)))) && (Integer.parseInt(cards.get(i).get(1))!=1))){
+                        if(cards.get(i+1).get(0)=="spade" || cards.get(i+1).get(0)=="heart" || cards.get(i+1).get(0)=="diamond" || ((cards.get(i+1).get(0)=="club") && (current > next))){
                             flag = 1;
                         }
+                        break;
+                    case "joker":
+                        flag = 1;
                         break;
                 }
                 if(flag==1){
@@ -159,7 +173,7 @@ class Carddeck {
     public static void sleep(int time){
         try {
             time=1;
-            Thread.sleep(time); 
+            Thread.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -172,6 +186,7 @@ class Trump5 extends Carddeck{
     public static List<List<String>> cpu2 = new ArrayList<>();
     public static List<List<String>> cpu3 = new ArrayList<>();
     public static List<List<String>> cpu4 = new ArrayList<>();
+    public static List<List<String>> exchange = new ArrayList<>();
 
     public static void main(String[] args) {
         initializeGame5();
@@ -201,6 +216,18 @@ class Trump5 extends Carddeck{
         cpu2 = list.subList(index, index += hand.get(2));
         cpu3 = list.subList(index, index += hand.get(3));
         cpu4 = list.subList(index, Math.min(index + hand.get(4), list.size()));
+    }
+
+    public static void initializeGame_napoleone() {
+        add_joker();
+
+        int index = 0;
+        player = list.subList(0, 10);
+        cpu1 = list.subList(10, 20);
+        cpu2 = list.subList(20, 30);
+        cpu3 = list.subList(30, 40);
+        cpu4 = list.subList(40, 50);
+        exchange = list.subList(50, 53);
     }
 }
 
@@ -318,7 +345,10 @@ class Old_Maid extends Trump5{
         System.out.print("Ranking: ");
         for(int i=0; i<5; i++){
             n = i+1;
-            System.out.print(n+"."+ ranking[i]+", ");
+            System.out.print(n+"."+ ranking[i]);
+            if(i != 4){
+                System.out.print(", ");
+            }
         }
     }
     public static void remove_none(List<List<String>> list){
@@ -396,8 +426,8 @@ class Poker extends Trump2{
         Map<String, List<List<String>>> players = trump.getplayers();
         List<List<String>> player = new ArrayList<>(players.get("player"));
         List<List<String>> cpu = new ArrayList<>(players.get("cpu"));
-        sote_cards(player);
-        sote_cards(cpu);
+        sort_cards(player);
+        sort_cards(cpu);
         System.out.print("Your hands: ");
         visual_card(player);
 
@@ -435,8 +465,8 @@ class Poker extends Trump2{
         int deck_top=10;
         if (super_flag == 0){
             for(int i=0; i<numbers.size(); i++){
-            player.add(list.get(deck_top));
-            deck_top++;
+                player.add(list.get(deck_top));
+                deck_top++;
             }
         }
         
@@ -454,7 +484,7 @@ class Poker extends Trump2{
             player.add(List.of("heart", "1"));
         }
 
-        sote_cards(player);
+        sort_cards(player);
         System.out.print("Your hands: ");
         visual_card(player);
         System.out.print("Cpu's hands: ");
@@ -672,6 +702,309 @@ class Poker extends Trump2{
         }
         return poker_hands;
     }
+}
+
+class Napoleon extends Trump5{
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        Trump5 trump = new Trump5();
+        int declaration;
+        trump.initializeGame_napoleone();
+        sort_cards_suit(player);
+        sort_cards_suit(cpu1);
+        sort_cards_suit(cpu2);
+        sort_cards_suit(cpu3);
+        sort_cards_suit(cpu4);
+        System.out.print("Player's first cards:");
+        visual_card(player);
+
+        Map<String, List<List<String>>> allPlayers = new LinkedHashMap<>();
+        allPlayers.put("Player", player);
+        allPlayers.put("cpu1", cpu1);
+        allPlayers.put("cpu2", cpu2);
+        allPlayers.put("cpu3", cpu3);
+        allPlayers.put("cpu4", cpu4);
+
+        List<String> declare_players = new ArrayList<>(allPlayers.keySet());
+
+        String napoleone="";
+        int[] cpu1_points = hands_point_count(cpu1);
+        int[] cpu2_points = hands_point_count(cpu2);
+        int[] cpu3_points = hands_point_count(cpu3);
+        int[] cpu4_points = hands_point_count(cpu4);
+        int[] current_declare = new int[2];
+        int[] current_declare_cpu = new int[2];
         
+        visual_card(cpu1);
+        System.out.println(cpu1_points[0] +", "+ cpu1_points[1]+", "+ cpu1_points[2]+", "+ cpu1_points[3]);
+        visual_card(cpu2);
+        System.out.println(cpu2_points[0] +", "+ cpu2_points[1]+", "+ cpu2_points[2]+", "+ cpu2_points[3]);
+        visual_card(cpu3);
+        System.out.println(cpu3_points[0] +", "+ cpu3_points[1]+", "+ cpu3_points[2]+", "+ cpu3_points[3]);
+        visual_card(cpu4);
+        System.out.println(cpu4_points[0] +", "+ cpu4_points[1]+", "+ cpu4_points[2]+", "+ cpu4_points[3]);
+        System.out.print("Are you declaring Napoleon? Press 1 for Yes, 2 for No:");
+        declaration = stdIn.nextInt();
+        if(declaration==1){
+            int declaration_cards;
+            int declaration_suit;
+            int declare_count = 11;
+            int declare_suit = 0;
+            int loop = 0;
+            String d_suit = "";
+            String print_suit = "";
+            System.out.print("Which suit do you declare? Press 1 for spade, 2 for heart, 3 for diamond, 4 for club: ");
+            declaration_suit = stdIn.nextInt();
+            switch(declaration_suit){
+                case 1:
+                    d_suit = "spade";
+                    break;
+                case 2:
+                    d_suit = "heart";
+                    break;
+                case 3:
+                    d_suit = "diamond";
+                    break;
+                case 4:
+                    d_suit = "club";
+                    break;
+            }
+            while(declare_players.size()>1){
+                switch(declare_suit){
+                    case 1:
+                        print_suit = "spade";
+                        break;
+                    case 2:
+                        print_suit = "heart";
+                        break;
+                    case 3:
+                        print_suit = "diamond";
+                        break;
+                    case 4:
+                        print_suit = "club";
+                        break;
+                }
+                for(String key : allPlayers.keySet()){
+                    if(declare_players.contains(key)){
+                        System.out.println(key);
+                        switch(key){
+                            case "player":
+                                if (loop == 1){
+                                    System.out.print(" The current declaration is "+ declare_count +" of "+ print_suit +". Do you continue to declare Napoleon? Press 1 for Yes, 2 for No:");
+                                    declaration = stdIn.nextInt();
+                                    if (declaration!=1){
+                                        declare_players.remove("player");
+                                        System.out.println("Pass");
+                                        break;
+                                    }
+                                }
+                                System.out.print("How many tricks do you declare? (More than "+ declare_count +"): ");
+                                declaration_cards = stdIn.nextInt();
+                                if(declaration_cards<declare_count){
+                                    declaration_cards=declare_count+1;
+                                }else if(declaration_cards>20){
+                                    declaration_cards=20;
+                                }
+                                System.out.println("player's declaration [ suit : "+ d_suit +", tricks : "+ declaration_cards +" ]");
+                                current_declare[0] = declaration_cards;
+                                current_declare[1] = declaration_suit;
+                                break;
+                            case "cpu1":
+                                current_declare_cpu=declare_cpu(cpu1_points, current_declare[0], current_declare[1]);
+                                break;
+                            case "cpu2":
+                                current_declare_cpu=declare_cpu(cpu2_points, current_declare[0], current_declare[1]);
+                                break;
+                            case "cpu3":
+                                current_declare_cpu=declare_cpu(cpu3_points, current_declare[0], current_declare[1]);
+                                break;
+                            case "cpu4":
+                                current_declare_cpu=declare_cpu(cpu4_points, current_declare[0], current_declare[1]);
+                                break;
+                        }
+                        if(current_declare_cpu[0]==-1){
+                            declare_players.remove(key);
+                        }else{
+                            current_declare=current_declare_cpu;
+                        }
+                        System.out.println(current_declare[0]+", "+ current_declare[1]);
+                    }
+                }
+                loop = 1;
+            }
+        }
+
+        //cpu 宣言
+        String adjutant = "";
+        String adjutant_suit = "";
+        int a_suit = 0;
+        int adjutant_num = 0;
+        if(napoleone=="player"){
+            System.out.println("Please declare the Adjutant's card.");
+            System.out.print("suit (Press 1 for spade, 2 for heart, 3 for diamond, 4 for club): ");
+            a_suit = stdIn.nextInt();
+            System.out.print("number: ");
+            adjutant_num = stdIn.nextInt();
+        }
+        switch(a_suit){
+                case 1:
+                    adjutant_suit = "spade";
+                    break;
+                case 2:
+                    adjutant_suit = "heart";
+                    break;
+                case 3:
+                    adjutant_suit = "diamond";
+                    break;
+                case 4:
+                    adjutant_suit = "club";
+                    break;
+        }
+
+        for (String key : allPlayers.keySet()) {
+            for(int j=0; j<allPlayers.get(key).size(); j++){
+                if(allPlayers.get(key).get(j).get(0) == adjutant_suit && Integer.parseInt(allPlayers.get(key).get(j).get(1)) == adjutant_num){
+                    adjutant=key;
+                }
+            }
+        }
+        
+        System.out.println(adjutant);
+        System.out.println("Napoleon may examine the cards in play and exchange any of them at will.");
+        if(napoleone=="player"){
+            
+            player.addAll(exchange);
+            
+            sort_cards_suit(player);
+            visual_card(player);
+            stdIn.nextLine();
+            System.out.print("Please choose and discard three cards. Please input the numbers from the left, separated by spaces:");
+            ArrayList<Integer> numbers = new ArrayList<>();
+
+            String input = stdIn.nextLine();        
+            String[] tokens = input.trim().split("\\s+");
+
+            for (int i = 0; i < 3; i++) {
+                try {
+                    int number = Integer.parseInt(tokens[i]);
+                    numbers.add(number-1);
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input: " + tokens[i]);
+                }
+            }
+            
+            Collections.sort(numbers);
+            for(int i=2; i>=0; i--){
+                int n = numbers.get(i);
+                player.remove(n);
+            }
+            
+            sort_cards_suit(player);
+            visual_card(player);
+
+
+        }
+    }
+    public static int[] hands_point_count(List<List<String>> cards){
+        int[] hands_point = new int[4];
+        int hand_suit = 0;
+        int hand_point = 0;
+        boolean j_flag = false;
+        boolean sa_flag = false;
+        for(int i=0; i<cards.size(); i++){
+            switch(cards.get(i).get(0)){
+                case "spade":
+                    hand_suit = 0;
+                    break;
+                case "heart":
+                    hand_suit = 1;
+                    break;
+                case "diamond":
+                    hand_suit = 2;
+                    break;
+                case "club":
+                    hand_suit = 3;
+                    break; 
+            }
+            if(cards.get(i).get(1)!="joker"){
+                switch(Integer.parseInt(cards.get(i).get(1))){
+                    case 1:
+                        hand_point = 3;
+                        if(hand_suit==0){
+                            sa_flag = true;
+                        }
+                    case 13:
+                    case 12:
+                    case 10:
+                        hand_point = 3;
+                        break;
+                    case 11:
+                        hand_point = 4;
+                        j_flag = true;
+                        break;
+                    case 9:
+                    case 8:
+                    case 7:
+                    case 6:
+                    case 5:
+                    case 4:
+                    case 3:
+                    case 2:
+                        hand_point = 2;
+                        break;
+                }
+            }
+            hands_point[hand_suit] += hand_point;
+            if(j_flag){
+                switch(hand_suit){
+                    case 0:
+                        hands_point[3] += 3;
+                        break;
+                    case 1:
+                        hands_point[2] += 3;
+                        break;
+                    case 2:
+                        hands_point[1] += 3;
+                        break;
+                    case 3:
+                        hands_point[0] += 3;
+                        break;
+                }
+                j_flag = false;
+            }
+            if(sa_flag){
+                for (int j = 0; j < hands_point.length; j++) {
+                    hands_point[j] += 1;
+                }
+                sa_flag = false;
+            }
+        }
+        return hands_point;
+    }
+    public static int[] declare_cpu(int[] hands_point, int declare_count, int declare_suit){
+        int[] declare_cpu = new int[2];
+        int max_value = hands_point[0];
+        int max_index = 0;
+
+        for (int i = 1; i < hands_point.length; i++) {
+            if (hands_point[i] > max_value) {
+                max_value = hands_point[i];
+                max_index = i;
+            }
+        }
+        if (max_value >= declare_count){
+            if(declare_suit > max_index){
+                declare_cpu[0] = max_index;
+                declare_cpu[1] = declare_count;
+            }else{
+                declare_cpu[0] = max_index;
+                declare_cpu[1] = declare_count+1;
+            }
+        }else{
+            declare_cpu[0] = -1;
+            declare_cpu[1] = -1;
+        }
+        return declare_cpu;
+    }
 }
 
